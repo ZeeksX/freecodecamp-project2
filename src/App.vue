61c379a8eecf052a1f3d34e891783f5ea4719fc4
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-09 15:53:56 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <div></div>
+  <div class="container">
+    <HomePage />
+  </div>
 </template>
-
 <script>
-
+import HomePage from "@/views/HomePage.vue"
 export default {
   name: 'App',
-  
+  components: {
+    HomePage
+  }
 }
 </script>
-
---
+++ src/components/CalculatorBody.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="calculator">
+        <div id="screen"></div>
+        <div id="display">0</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "CalculatorBody"
+}
+</script>
