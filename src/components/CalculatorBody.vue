70fa73428e53ed9cf9bba71ea100d4ca3c8bffa8
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-15 23:13:57 +0100

    changes

--- src/components/CalculatorBody.vue
+++ src/components/CalculatorBody.vue
@@ -1,70 +1,70 @@
 <template>
-        <div class="calculator">
-            <div id="formula">{{ calculatorStore.formula }}</div>
-            <div id="result">{{ calculatorStore.text }}</div>
-            <div class="row" id="row1">
-                <div class="calc" id="clear" @click="clearData">
-                    <div class="button-wrapper">AC</div>
-                </div>
-                <div class="calc oper" id="division" @click="append('/')">
-                    <div class="button-wrapper">/</div>
-                </div>
-                <div class="calc oper" id="multiplication" @click="append('*')">
-                    <div class="button-wrapper">x</div>
-                </div>
+    <div class="calculator">
+        <div id="formula">{{ calculatorStore.formula }}</div>
+        <div id="result">{{ calculatorStore.text }}</div>
+        <div class="row" id="row1">
+            <div class="calc" id="clear" @click="clearData">
+                <div class="button-wrapper">AC</div>
             </div>
-            <div class="row" id="row2">
-                <div class="calc num" id="seven" @click="append('7')">
-                    <div class="button-wrapper">7</div>
-                </div>
-                <div class="calc num" id="eight" @click="append('8')">
-                    <div class="button-wrapper">8</div>
-                </div>
-                <div class="calc num" id="nine" @click="append('9')">
-                    <div class="button-wrapper">9</div>
-                </div>
-                <div class="calc oper" id="subtraction" @click="append('-')">
-                    <div class="button-wrapper">-</div>
-                </div>
+            <div class="calc oper" id="division" @click="append('/')">
+                <div class="button-wrapper">/</div>
             </div>
-            <div class="row" id="row3">
-                <div class="calc num" id="four" @click="append('4')">
-                    <div class="button-wrapper">4</div>
-                </div>
-                <div class="calc num" id="five" @click="append('5')">
-                    <div class="button-wrapper">5</div>
-                </div>
-                <div class="calc num" id="six" @click="append('6')">
-                    <div class="button-wrapper">6</div>
-                </div>
-                <div class="calc oper" id="addition" @click="append('+')">
-                    <div class="button-wrapper">+</div>
-                </div>
-            </div>
-            <div class="row" id="row4">
-                <div class="calc num" id="one" @click="append('1')">
-                    <div class="button-wrapper">1</div>
-                </div>
-                <div class="calc num" id="two" @click="append('2')">
-                    <div class="button-wrapper">2</div>
-                </div>
-                <div class="calc num" id="three" @click="append('3')">
-                    <div class="button-wrapper">3</div>
-                </div>
-                <div class="calc oper" id="equals" @click="computeResult('=')"
-                    :class="{ 'disabled': calculationCompleted }">
-                    <div class="button-wrapper">=</div>
-                </div>
-            </div>
-            <div class="row" id="row5">
-                <div class="calc num" id="zero" @click="append('0')">
-                    <div class="button-wrapper">0</div>
-                </div>
-                <div class="calc num" id="dot" @click="append('.')">
-                    <div class="button-wrapper">.</div>
-                </div>
+            <div class="calc oper" id="multiplication" @click="append('*')">
+                <div class="button-wrapper">x</div>
             </div>
         </div>
+        <div class="row" id="row2">
+            <div class="calc num" id="seven" @click="append('7')">
+                <div class="button-wrapper">7</div>
+            </div>
+            <div class="calc num" id="eight" @click="append('8')">
+                <div class="button-wrapper">8</div>
+            </div>
+            <div class="calc num" id="nine" @click="append('9')">
+                <div class="button-wrapper">9</div>
+            </div>
+            <div class="calc oper" id="subtraction" @click="append('-')">
+                <div class="button-wrapper">-</div>
+            </div>
+        </div>
+        <div class="row" id="row3">
+            <div class="calc num" id="four" @click="append('4')">
+                <div class="button-wrapper">4</div>
+            </div>
+            <div class="calc num" id="five" @click="append('5')">
+                <div class="button-wrapper">5</div>
+            </div>
+            <div class="calc num" id="six" @click="append('6')">
+                <div class="button-wrapper">6</div>
+            </div>
+            <div class="calc oper" id="addition" @click="append('+')">
+                <div class="button-wrapper">+</div>
+            </div>
+        </div>
+        <div class="row" id="row4">
+            <div class="calc num" id="one" @click="append('1')">
+                <div class="button-wrapper">1</div>
+            </div>
+            <div class="calc num" id="two" @click="append('2')">
+                <div class="button-wrapper">2</div>
+            </div>
+            <div class="calc num" id="three" @click="append('3')">
+                <div class="button-wrapper">3</div>
+            </div>
+            <div class="calc oper" id="equals" @click="computeResult('=')"
+                :class="{ 'disabled': calculationCompleted }">
+                <div class="button-wrapper">=</div>
+            </div>
+        </div>
+        <div class="row" id="row5">
+            <div class="calc num" id="zero" @click="append('0')">
+                <div class="button-wrapper">0</div>
+            </div>
+            <div class="calc num" id="dot" @click="append('.')">
+                <div class="button-wrapper">.</div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -80,39 +80,39 @@ export default {
         };
     },
     methods: {
-        append(label) {
+       append(label) {
             if (!this.calculationCompleted && this.calculatorStore.text !== "DIGIT LIMIT MET") {
                 if (this.calculatorStore.text === "0" || this.isOperator(this.calculatorStore.text)) {
                     this.calculatorStore.text = label;
                     this.lastOperator = label;
+                    if (!this.isOperator(label)) {
+                        this.calculatorStore.formula += label;
+                    } else {
+                        if (this.isOperator(this.lastOperator) && label !== "-") {
+                            this.calculatorStore.formula = this.calculatorStore.formula.slice(0, -1) + label;
+                        } else {
+                            this.calculatorStore.text = label;
+                            this.calculatorStore.formula += label;
+                        }
+                    }
                 } else {
                     if (this.isOperator(label)) {
-                        if (this.isOperator(this.lastOperator) && this.lastOperator !== label && label !== "-") {
-                            if (this.lastOperator == "+" || this.lastOperator == "/" || this.lastOperator == "*") {
-                                this.calculatorStore.text = label;
-                                this.lastOperator = label;
-                                this.calculatorStore.formula = this.calculatorStore.formula.slice(0, -1) + label;
-                            } else {
-                                this.calculatorStore.formula += label
-                            }
-
+                        // Replace previous operator if it's not "-"
+                        if (this.isOperator(this.lastOperator) && label !== "-") {
+                            this.calculatorStore.formula = this.calculatorStore.formula.slice(0, -1) + label;
                         } else {
-                            this.calculatorStore.text = label
-                            this.calculatorStore.formula += label
+                            this.calculatorStore.text = label;
+                            this.calculatorStore.formula += label;
                         }
+                        this.lastOperator = label;
                     } else {
+                        // Append the number to the text and formula
                         this.calculatorStore.text += label;
-
-                        this.lastOperator = null; 
-                    }
-
-                }
-                if (this.calculatorStore.formula.slice(-1) !== label) {
-                    if (this.lastOperator == "+" || this.lastOperator == "/" || this.lastOperator == "*") {
-                        this.calculatorStore.formula = this.calculatorStore.formula.slice(0, -1) + label;
-                    } else {
-
                         this.calculatorStore.formula += label;
+                        // If the last character was an operator, reset lastOperator to null
+                        // if (this.isOperator(this.lastOperator)) {
+                        //     this.lastOperator = null;
+                        // }
                     }
                 }
             }
